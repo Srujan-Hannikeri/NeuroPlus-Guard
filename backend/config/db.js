74bd42873d26f2f9a4a09da934ea96d144e34fe5
Srujan Hgit config --global user.name Srujan <srujanhannikeri@gmail.com>
2026-05-21 18:54:59 +0530

    Implement Serverless Database Connection Pooling for high traffic scaling

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 
+let isConnected;
+
 const connectDB = async () => {
+  if (isConnected) {
+    console.log('=> Using existing database connection');
+    return;
+  }
+  
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    const db = await mongoose.connect(process.env.MONGO_URI, {
+      maxPoolSize: 50, // Maintain up to 50 socket connections
+      serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    });
+    isConnected = db.connections[0].readyState;
+    console.log(`MongoDB Connected: ${db.connection.host}`);
   } catch (error) {
     console.error(`MongoDB Connection Error: ${error.message}`);
   }
